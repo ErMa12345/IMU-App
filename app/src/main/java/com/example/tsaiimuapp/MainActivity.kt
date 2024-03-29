69d0aa3149003c976a24715f3a9188534cd6852f
Eric Ma <er11cma2004@gmail.com>
2024-03-29 16:35:57 -0400

    Added app files

---
+++ app/src/main/java/com/example/tsaiimuapp/MainActivity.kt
@@ -0,0 +1,150 @@
+package com.example.tsaiimuapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.UiComposable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tsaiimuapp.ui.theme.TsaiIMUAppTheme
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.math.BigDecimal
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+
+import androidx.compose.runtime.*
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+
+
+//Global arrays that will be used to graph data
+val zeroArray = FloatArray(100) { 0.0f }
+val timeArray = Array<BigDecimal>(100) { BigDecimal.ZERO }
+
+var time = mutableStateOf(ArrayList<BigDecimal>(timeArray.asList()))
+var yaw = mutableStateOf(ArrayList<Float>(zeroArray.asList()))
+var pitch = mutableStateOf(ArrayList<Float>(zeroArray.asList()))
+var roll = mutableStateOf(ArrayList<Float>(zeroArray.asList()))
+var accelX = mutableStateOf(ArrayList<Float>(zeroArray.asList()))
+var accelY = mutableStateOf(ArrayList<Float>(zeroArray.asList()))
+var accelZ = mutableStateOf(ArrayList<Float>(zeroArray.asList()))
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            TsaiIMUAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen(readCSV = ::readCSV)
+
+                }
+            }
+        }
+
+
+    }
+
+    /**
+     * Reads the CSV and updates the value in the global arrays as they are read in
+     */
+    suspend fun readCSV() {
+        withContext(Dispatchers.IO) {
+            val file = assets.open("e2e_ref.csv")
+            val isr = InputStreamReader(file)
+            val reader = BufferedReader(isr)
+
+            var line = reader.readLine()
+
+            var counter = 0
+
+            while (line != null) {
+                //get current line values
+                line = reader.readLine()
+                var current : List<String> = line.split(',')
+                var curTime = BigDecimal(current[1]) - BigDecimal(1666302376558)
+                var curAccelX = current[2].toFloat()
+                var curAccelY = current[3].toFloat()
+                var curAccelZ = current[4].toFloat()
+                var curYaw = current[12].toFloat()
+                var curPitch = current[13].toFloat()
+                var curRoll = current[14].toFloat()
+
+                //update the global lists
+//                time.removeFirst()
+//                time.addLast(curTime)
+//
+//                accelX.removeFirst()
+//                accelX.addLast(curAccelX)
+//
+//                accelY.removeFirst()
+//                accelY.addLast(curAccelY)
+//
+//                accelZ.removeFirst()
+//                accelZ.addLast(curAccelZ)
+//
+//                yaw.removeFirst()
+//                yaw.addLast(curYaw)
+//
+//                pitch.removeFirst()
+//                pitch.addLast(curPitch)
+//
+//                roll.removeFirst()
+//                roll.addLast(curRoll)
+
+                time.value = (time.value.drop(1) + curTime) as ArrayList<BigDecimal>
+
+                accelX.value = (accelX.value.drop(1) + curAccelX) as ArrayList<Float>
+
+                accelY.value = (accelY.value.drop(1) + curAccelY) as ArrayList<Float>
+
+                accelZ.value = (accelZ.value.drop(1) + curAccelZ) as ArrayList<Float>
+
+                yaw.value = (yaw.value.drop(1) + curYaw) as ArrayList<Float>
+
+                pitch.value = (pitch.value.drop(1) + curPitch) as ArrayList<Float>
+
+                roll.value = (roll.value.drop(1) + curRoll) as ArrayList<Float>
+
+                //Print data every 100 updates, for debugging, delete later
+                counter++
+
+
+                //update every 100 ms (about how long it takes between updates in the csv)
+                Thread.sleep(100)
+
+        }
+
+
+        }
+    }
+
+
+}
+
+
+
---
+++ app/src/main/java/com/example/tsaiimuapp/MainScreen.kt
@@ -0,0 +1,178 @@
+package com.example.tsaiimuapp
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
+import kotlinx.coroutines.launch
+import java.math.BigDecimal
+
+var launched = 0
+@SuppressLint("RememberReturnType")
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(readCSV: suspend () -> Unit) {
+    val scope = rememberCoroutineScope()
+    val pagerState = rememberPagerState(pageCount = {HomeTabs.entries.size})
+    val selectedTabIndex = remember {
+        derivedStateOf { pagerState.currentPage }
+    }
+
+    val curScreen = remember {
+        mutableStateOf(pagerState.currentPage)
+    }
+    LaunchedEffect(Unit) {
+        readCSV()
+    }
+    Scaffold (
+        topBar = { TopAppBar(title = { Text(text = "IMU App")})}
+    ) {
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = it.calculateTopPadding())
+        ) {
+            TabRow(
+                selectedTabIndex = selectedTabIndex.value,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                HomeTabs.entries.forEachIndexed {index, currentTab ->
+                    Tab(
+                        selected = selectedTabIndex.value == index,
+                        onClick = {
+                            scope.launch {
+                                pagerState.animateScrollToPage(currentTab.ordinal)
+                                curScreen.value = currentTab.ordinal
+                            }
+                        },
+                        text = {
+                            Text(text = currentTab.text)
+
+                        }
+                    )
+                }
+            }
+            HorizontalPager(
+                state = pagerState,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                Box (
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    //Text(text = "works")
+                    if (curScreen.value == 0) {
+                        graphArrays(time = time, lineOne = yaw, lineTwo = pitch, lineThree = roll)
+                    } else {
+                        graphArrays(time = time, lineOne = accelX, lineTwo = accelY, lineThree = accelZ)
+                    }
+                }
+            }
+        }
+    }
+
+}
+@Composable
+fun graphArrays (time: MutableState<ArrayList<BigDecimal>>, lineOne: MutableState<ArrayList<Float>>, lineTwo: MutableState<ArrayList<Float>>, lineThree: MutableState<ArrayList<Float>>) {
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        val minValue = listOf(lineOne.value, lineTwo.value, lineThree.value).flatten().minOrNull() ?: 0f
+        val maxValue = listOf(lineOne.value, lineTwo.value, lineThree.value).flatten().maxOrNull() ?: 0f
+        val scaleFactor = (maxValue - minValue) / size.height
+
+        fun scaleValue(value: Float): Float {
+            return (value - minValue) / scaleFactor
+        }
+
+        val points1 = lineOne.value.mapIndexed {
+                index, value -> Offset(x = size.width * index / (lineOne.value.size - 1), y = scaleValue(value))
+        }
+
+        val points2 = lineTwo.value.mapIndexed {
+                index, value -> Offset(x = size.width * index / (lineTwo.value.size - 1), y = scaleValue(value))
+        }
+
+        val points3 = lineThree.value.mapIndexed {
+                index, value -> Offset(x = size.width * index / (lineThree.value.size - 1), y = scaleValue(value))
+        }
+
+
+
+        //actual lines
+
+        //line1
+        drawPath(
+            path = Path().apply {
+                moveTo(points1.first().x, points1.first().y)
+                points1.forEach { point ->
+                    lineTo(point.x, point.y)
+                }
+            },
+            color = Color.Blue,
+            style = Stroke(width = 4f)
+        )
+
+        //line2
+        drawPath(
+            path = Path().apply {
+                moveTo(points2.first().x, points2.first().y)
+                points2.forEach { point ->
+                    lineTo(point.x, point.y)
+                }
+            },
+            color = Color.Red,
+            style = Stroke(width = 4f)
+        )
+
+        //line3
+        drawPath(
+            path = Path().apply {
+                moveTo(points3.first().x, points3.first().y)
+                points3.forEach { point ->
+                    lineTo(point.x, point.y)
+                }
+            },
+            color = Color.Green,
+            style = Stroke(width = 4f)
+        )
+
+
+    }
+}
+enum class HomeTabs (
+    val text: String
+) {
+    Gyroscope(
+        text = "Gyroscope"
+    ),
+    Accelerometer (
+        text = "Accelerometer"
+    )
+
+}
